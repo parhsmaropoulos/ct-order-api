6d207c1bf352ae1cca13fc0d12f34895c438ea54
paris maropoulos <pmaropoulos@skroutzdelivery.gr>
2021-09-30 10:27:01 +0300

    Set different admin auth for backend

--- backend/config/firebase.go
+++ backend/config/firebase.go
@@ -29,3 +29,25 @@ func SetupFirebase() *auth.Client {
 	}
 	return auth
 }
+
+
+
+func SetupAdminFirebase() *auth.Client {
+	serviceAccountKeyFilePath, err := filepath.Abs("./config/adminServiceAccountKey.json")
+	if err != nil {
+		panic("Unable to load adminServiceAccountKey.json file")
+	}
+	opt := option.WithCredentialsFile(serviceAccountKeyFilePath)
+	//Firebase admin SDK initialization
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		panic("Firebase load error")
+	}
+	//Firebase Auth
+	auth, err := app.Auth(context.Background())
+	fmt.Println(err)
+	if err != nil {
+		panic("Firebase load error")
+	}
+	return auth
+}
