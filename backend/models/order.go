efe02dfd8ec604178eb571bcfdacf81780ac23ee
paris maropoulos <pmaropoulos@skroutzdelivery.gr>
2021-09-30 10:32:14 +0300

    Added function to generate pdf reciept for order

--- backend/models/order.go
+++ backend/models/order.go
@@ -2,10 +2,17 @@
 package models
 
 import (
+	"bytes"
 	"database/sql/driver"
 	"encoding/json"
 	"errors"
+	"fmt"
+	"math"
 
+	"github.com/johnfercher/maroto/pkg/color"
+	"github.com/johnfercher/maroto/pkg/consts"
+	"github.com/johnfercher/maroto/pkg/pdf"
+	"github.com/johnfercher/maroto/pkg/props"
 	"github.com/lib/pq"
 	"gorm.io/gorm"
 )
@@ -17,7 +24,7 @@ type OrderProduct struct {
 	Extra_Ingredients pq.StringArray `json:"extra_ingredients" gorm:"type:varchar[]"`
 	Item_Name         string         `json:"item_name"`
 	Option_Answers    pq.StringArray `json:"option_answers" gorm:"type:varchar[]"`
-	Quantity          int32          `json:"quantity"`
+	Quantity          int32         `json:"quantity"`
 	Total_Price       float64        `json:"total_price"`
 	// Options []struct {
 	// 	Name   string  `json:"name"`
@@ -37,6 +44,7 @@ func (c *OrderProduct) Scan(value interface{}) error {
 	}
 	return json.Unmarshal(b, &c)
 }
+
 
 type OrderProducts []OrderProduct
 
@@ -88,4 +96,294 @@ type Order struct {
 	Accepted  bool `json:"accepted"`
 	Completed bool `json:"completed"`
 	Canceled  bool `json:"canceled"`
+
+	Receipt bytes.Buffer `json:"receipt" gorm:"-"`
+}
+
+func Round(num float64) int {
+    return int(num + math.Copysign(0.5, num))
+}
+
+func ToFixed(num float64, precision int) float64 {
+    output := math.Pow(10, float64(precision))
+    return float64(Round(num * output)) / output
+}
+
+func GeneratePDFReciept(order Order) bytes.Buffer {
+	// begin := time.Now()
+
+	// darkGrayColor := getDarkGrayColor()
+	// grayColor := getGrayColor()
+	// whiteColor := color.NewWhite()
+	// blueColor := getBlueColor()
+	// redColor := getRedColor()
+	// header := getHeader()
+
+	m := pdf.NewMaroto(consts.Portrait, consts.A5)
+	// m.SetPageMargins(10, 15, 10)
+// HEADER
+	m.RegisterHeader(func() {
+		m.Row(20, func() {
+			m.Col(12,func() {
+				m.Text("COFFEE TWIST", props.Text{
+					Align:       consts.Left,
+					Extrapolate: false,
+				})
+				m.Text("ΠΡΟΣΟΧΗ ΔΕΝ ΕΙΝΑΙ ΑΠΟΔΕΙΞΗ", props.Text{
+					Top: 5,
+					Align:       consts.Left,
+					Extrapolate: false,
+				})
+				m.Text("***** ΔΕΛΤΙΟ ΠΑΡΑΓΓΕΛΙΑΣ *****", props.Text{
+					Top: 10,
+					Align:       consts.Left,
+					Extrapolate: false,
+				})
+				m.Text(fmt.Sprintf("%d-%d-%d  %d-%d-%d  ", order.CreatedAt.Day(),order.CreatedAt.Month(),order.CreatedAt.Year(),order.CreatedAt.Hour(),order.CreatedAt.Minute(),order.CreatedAt.Second()), props.Text{
+					Top: 15,
+					Align:       consts.Left,
+					Extrapolate: false,
+				})
+				m.Text(fmt.Sprintf("ΑΡΙΘΜΟΣ ΠΑΡΑΓΓΕΛΙΑΣ %d ",order.ID), props.Text{
+
+					Top: 20,
+					Align:       consts.Left,
+					Extrapolate: false,
+				})
+			})
+		})
+	})
+// FOOTER
+	m.RegisterFooter(func() {
+		m.Row(20, func() {
+			m.Col(12, func() {
+				m.Text("##### www.coffeetwist.gr #####", props.Text{
+					Top:   16,
+					Size:  8,
+					Align: consts.Center,
+				})
+			})
+		})
+	})
+// ORDER TIPS
+	m.Row(20, func() {
+		if order.Tips > 0.1 {
+			m.Col(3, func() {
+				m.Text("TIP Διανομέα :", props.Text{
+					Top:   16,
+					Style: consts.Bold,
+					Align: consts.Left,
+				})
+			})
+			m.Col(6, func() {
+				m.Text(fmt.Sprintf("%.2f  €", order.Tips), props.Text{
+					Top:   16,
+					Style: consts.Bold,
+					Align: consts.Right,
+				})
+			})
+		}
+	})
+// ORDER PRODUCTS
+	for i, prod := range order.Products{
+		m.Row(10, func() {
+			m.Row(6, func() {
+				m.Text(fmt.Sprintf("%d x %s", prod.Quantity,prod.Item_Name), props.Text{
+					Top:   3,
+					Style: consts.Bold,
+					Align: consts.Left,
+				})
+		})
+		if len(prod.Option_Answers) > 0 {
+			m.Row(10,func() {
+				//map answers
+				for _,answer := range prod.Option_Answers {
+					m.Col(6, func() {
+						m.Text(fmt.Sprintf("  +%s",answer), props.Text{
+							Top:   3 + float64(i)*3,
+							Style: consts.Bold,
+							Align: consts.Left,
+						})
+					})
+				}
+			})
+		}
+		if len(prod.Extra_Ingredients) > 0 {
+			m.Row(10,func() {
+				//map extra ingredients
+				for i,extra := range prod.Extra_Ingredients {
+					m.Col(6, func() {
+						m.Text(fmt.Sprintf("  +%s",extra), props.Text{
+							Top:   3 + float64(i)*3,
+							Style: consts.Bold,
+							Align: consts.Left,
+						})
+					})
+				}
+			})
+		}
+		if prod.Comment != "" {
+			m.Row(10,func() {
+				//map extra ingredients
+				m.Col(6, func() {
+					m.Text(fmt.Sprintf(" Σχόλιο +%s",prod.Comment), props.Text{
+						Top:   3,
+						Style: consts.Bold,
+						Align: consts.Left,
+					})
+				})
+			})
+		}
+		m.Col(3, func() {
+			m.Text(fmt.Sprintf("%.2f €",prod.Total_Price), props.Text{
+				Top:   5,
+				Style: consts.Bold,
+				Align: consts.Right,
+			})
+		})
+	})
+}
+// ORDER COMMENTS
+	if order.Comments != ""{
+		m.Row(20, func() {
+			m.Col(2, func() {
+				m.Text("Σχόλια:", props.Text{
+					Top:   5,
+					Style: consts.Bold,
+					Size:  8,
+					Align: consts.Left,
+				})
+			})
+			m.Col(10, func() {
+				m.Text(order.Comments, props.Text{
+					Top:   5,
+					Style: consts.Bold,
+					Size:  8,
+					Align: consts.Left,
+				})
+			})
+		})
+	}
+
+	// PAYMENT TYPE AND TOTAL
+	m.Row(20,func() {
+		m.Col(12,func() {
+			m.Text("#######################################",props.Text{
+				Top:5,
+				Style: consts.Bold,
+				Align: consts.Center,
+			})
+			m.Text(fmt.Sprintf("ΕΞΟΦΛΗΣΗ ΜΕ: %s",order.Payment_Type),props.Text{
+				Top:10,
+				Style: consts.Bold,
+				Align: consts.Center,
+			})
+			m.Text("ΣΥΝΟΛΟ:", props.Text{
+				Top:   15,
+				Style: consts.Bold,
+				Align: consts.Center,
+			})
+			m.Text(fmt.Sprintf("%.2f €",order.After_Discount_Price), props.Text{
+				Top:   15,
+				Style: consts.Bold,
+				Align: consts.Right,
+			})
+			m.Text("#######################################",props.Text{
+				Top:20,
+				Style: consts.Bold,
+				Align: consts.Center,
+			})
+		})
+	})
+	// USER DETAILS
+
+	m.Row(25, func() {
+		m.Col(12,func() {
+			m.Text(fmt.Sprintf("ΟΝ/ΜΟ : %s %s",order.Name ,order.Surname), props.Text{
+				Top:3,
+				Align:       consts.Left,
+				Extrapolate: false,
+			})
+			m.Text(fmt.Sprintf("ΚΙΝΗΤΟ : %d",order.Phone), props.Text{
+				Top: 6,
+				Align:       consts.Left,
+				Extrapolate: false,
+			})
+			m.Text(fmt.Sprintf("ΠΟΛΗ : %s, %s",order.Client_Area_Name, order.Client_City_Name), props.Text{
+				Top: 9,
+				Align:       consts.Left,
+				Extrapolate: false,
+			})
+			m.Text(fmt.Sprintf("ΟΔΟΣ : %s %s",order.Client_Address_Name, order.Client_Address_Number), props.Text{
+				Top: 12,
+				Align:       consts.Left,
+				Extrapolate: false,
+			})
+			m.Text(fmt.Sprintf("ΤΚ : %s",order.Client_Zip), props.Text{
+				Top: 15,
+				Align:       consts.Left,
+				Extrapolate: false,
+			})
+			m.Text(fmt.Sprintf("ΟΡΟΦΟΣ : %s",order.Floor), props.Text{
+				Top: 18,
+				Align:       consts.Left,
+				Extrapolate: false,
+			})
+			m.Text(fmt.Sprintf("ΚΟΥΔΟΥΝΙ : %s",order.Bell_name), props.Text{
+				Top: 21,
+				Align:       consts.Left,
+				Extrapolate: false,
+			})
+		})
+	})
+
+
+	output,err := m.Output()
+	// err := m.OutputFileAndClose(fmt.Sprintf("assets/pdfs/order-%d.pdf",order.ID))
+	if err != nil {
+		fmt.Println("Could not save PDF:", err)
+		return bytes.Buffer{}
+	}
+	return output
+
+	// end := time.Now()
+	// fmt.Println(end.Sub(begin))
+}
+
+func getHeader() []string {
+	return []string{"Quantity", "Name", "Price"}
+}
+
+
+
+func getDarkGrayColor() color.Color {
+	return color.Color{
+		Red:   55,
+		Green: 55,
+		Blue:  55,
+	}
+}
+
+func getGrayColor() color.Color {
+	return color.Color{
+		Red:   200,
+		Green: 200,
+		Blue:  200,
+	}
+}
+
+func getBlueColor() color.Color {
+	return color.Color{
+		Red:   10,
+		Green: 10,
+		Blue:  150,
+	}
+}
+
+func getRedColor() color.Color {
+	return color.Color{
+		Red:   150,
+		Green: 10,
+		Blue:  10,
+	}
 }
